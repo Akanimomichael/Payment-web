77360ff58d0a4f3fe5fb626b2a3ee65c9f2c7887
Akanimomike <101297309+Akanimomichael@users.noreply.github.com>
2022-07-11 00:04:16 +0100

    NAV BAR

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const hamburger = document.querySelector(".toggler");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        // link.classList.toggle("fade");
+    });
+
+    //toggler Animation
+    toggler.classList.toggle("toggle");
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
